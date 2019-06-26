ad2714a53110244f95c15251c0abc72260781307
Wes Huang <Wes.Huang@moxa.com>
2019-06-26 20:59:32 +0800

    feat: add a new option style

--- tools/mx-dio-ctl.c
+++ tools/mx-dio-ctl.c
@@ -27,7 +27,13 @@ enum action_type {
 	SET_DOUT = 2
 };
 
+enum action_version {
+	ACTION_VER_OLD = 0,
+	ACTION_VER_NEW = 1
+};
+
 struct action_struct {
+	int version;
 	int type;
 	int port;
 	int state;
@@ -36,25 +42,25 @@ struct action_struct {
 void usage(FILE *fp)
 {
 	fprintf(fp, "Usage:\n");
-	fprintf(fp, "	mx-dio-ctl -g <DOUT/DIN>|-s <state> -n <port>\n\n");
+	fprintf(fp, "	mx-dio-ctl <-i|-o <#port number> [-s <#state>]>\n\n");
 	fprintf(fp, "OPTIONS:\n");
-	fprintf(fp, "	-g <DOUT/DIN>\n");
-	fprintf(fp, "		Get target to DOUT or DIN port\n");
-	fprintf(fp, "		0 --> DOUT\n");
-	fprintf(fp, "		1 --> DIN\n");
-	fprintf(fp, "	-s <state>\n");
+	fprintf(fp, "	-i <#DIN port number>\n");
+	fprintf(fp, "	-o <#DOUT port number>\n");
+	fprintf(fp, "	-s <#state>\n");
 	fprintf(fp, "		Set state for target DOUT port\n");
 	fprintf(fp, "		0 --> LOW\n");
 	fprintf(fp, "		1 --> HIGH\n");
-	fprintf(fp, "	-n <port>\n");
-	fprintf(fp, "		Set target port number\n");
 	fprintf(fp, "\n");
 	fprintf(fp, "Example:\n");
-	fprintf(fp, "	Get value from DIN port 1\n");
-	fprintf(fp, "	# mx-dio-ctl -g 1 -n 1\n");
+	fprintf(fp, "	Get value from DIN port 0\n");
+	fprintf(fp, "	# mx-dio-ctl -i 0\n");
+	fprintf(fp, "	Get value from DOUT port 0\n");
+	fprintf(fp, "	# mx-dio-ctl -o 0\n");
 	fprintf(fp, "\n");
-	fprintf(fp, "	Set DOUT port 2 value to LOW\n");
-	fprintf(fp, "	# mx-dio-ctl -s 0 -n 2\n");
+	fprintf(fp, "	Set DOUT port 0 value to LOW\n");
+	fprintf(fp, "	# mx-dio-ctl -o 0 -s 0\n");
+	fprintf(fp, "	Set DOUT port 0 value to HIGH\n");
+	fprintf(fp, "	# mx-dio-ctl -o 0 -s 1\n");
 }
 
 int my_atoi(const char *nptr, int *number)
@@ -104,6 +110,7 @@ void do_action(struct action_struct action)
 int main(int argc, char *argv[])
 {
 	struct action_struct action = {
+		.version = UNSET,
 		.type = UNSET,
 		.port = UNSET,
 		.state = UNSET
@@ -111,7 +118,7 @@ int main(int argc, char *argv[])
 	int c;
 
 	while (1) {
-		c = getopt(argc, argv, "hg:s:n:");
+		c = getopt(argc, argv, "hg:s:n:i:o:");
 		if (c == -1)
 			break;
 
@@ -120,6 +127,12 @@ int main(int argc, char *argv[])
 			usage(stdout);
 			exit(0);
 		case 'g':
+			if (action.version != UNSET) {
+				fprintf(stderr, "action has already set\n");
+				usage(stderr);
+				exit(99);
+			}
+			action.version = ACTION_VER_OLD;
 			if (action.type != UNSET) {
 				fprintf(stderr, "action has already set\n");
 				usage(stderr);
@@ -130,9 +143,40 @@ int main(int argc, char *argv[])
 				exit(1);
 			}
 			break;
-		case 's':
-			if (action.type != UNSET) {
+		case 'i':
+			if (action.version != UNSET) {
 				fprintf(stderr, "action has already set\n");
+				usage(stderr);
+				exit(99);
+			}
+			action.version = ACTION_VER_NEW;
+			action.type = GET_DIN;
+			if (my_atoi(optarg, &action.port) != 0) {
+				fprintf(stderr, "%s is not a number\n", optarg);
+				exit(1);
+			}
+			break;
+		case 'o':
+			if (action.version != UNSET) {
+				fprintf(stderr, "action has already set\n");
+				usage(stderr);
+				exit(99);
+			}
+			action.version = ACTION_VER_NEW;
+			action.type = GET_DOUT;
+			if (my_atoi(optarg, &action.port) != 0) {
+				fprintf(stderr, "%s is not a number\n", optarg);
+				exit(1);
+			}
+			break;
+		case 's':
+			if (action.type != UNSET && action.version == ACTION_VER_OLD) {
+				fprintf(stderr, "action has already set\n");
+				usage(stderr);
+				exit(99);
+			}
+			if (action.type == GET_DIN && action.version == ACTION_VER_NEW) {
+				fprintf(stderr, "can't set di state\n");
 				usage(stderr);
 				exit(99);
 			}
